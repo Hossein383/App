39c7081b991864164489695c17789b6679b31bb3
Hossein <91972306+Hossein383@users.noreply.github.com>
2025-12-23 23:46:01 +0330

    Update build.gradle.kts

--- Firenet/app/build.gradle.kts
+++ Firenet/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 1004
-        versionName = "1.0.1"
+        versionName = "1.5.0"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
