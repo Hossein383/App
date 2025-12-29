0f76c4a4efb067eadc960be9ae264271107b206e
Hossein <91972306+Hossein383@users.noreply.github.com>
2025-12-29 12:54:58 +0330

    Update MainActivity.kt

--- Firenet/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ Firenet/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -138,7 +138,6 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        com.blongho.country_data.World.init(this) // فعال‌سازی کتابخانه پرچم
         title = getString(R.string.title_server)
 
         binding.btnMenu.setOnClickListener {
