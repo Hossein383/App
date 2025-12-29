daeb64ea73ef3490496e3a9f361f8b1d48150696
Hossein <91972306+Hossein383@users.noreply.github.com>
2025-12-29 12:37:39 +0330

    Update build.gradle.kts

--- Firenet/app/build.gradle.kts
+++ Firenet/app/build.gradle.kts
@@ -224,8 +224,3 @@ dependencies {
     implementation("com.github.blongho:worldCountryData:1.5.4")
     implementation("com.google.android.material:material:1.12.0")
 }
-repositories {
-    google()
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
-}
